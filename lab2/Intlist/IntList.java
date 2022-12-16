4a8219aafe5845666d57e88a24a9d3b866e0df2b
lumingfan <2507550027@qq.com>
2022-12-16 15:19:30 +0800

    finish lab2: add the correct function dcatenate and catenate in IntList.java

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,15 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+        if (A.rest == null) {
+            A.rest = B;
+            return A;
+        }
+        A.rest = dcatenate(A.rest, B);
+        return A;
     }
 
     /**
@@ -91,7 +99,13 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null && B == null) {
+            return null;
+        }
+        if (A == null) {
+            return new IntList(B.first, catenate(null, B.rest));
+        }
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
