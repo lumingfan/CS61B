44d09ad04d5b9ce73b769d1fa34552f13c6c2db1
lumingfan <2507550027@qq.com>
2022-12-19 17:11:58 +0800

    second commit: for handle the NullPointerExceptions, tranvert the int to Integer

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,19 +6,19 @@ public class TestArrayDequeGold {
     public void testDeque() {
         StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> right = new ArrayDequeSolution<>();
-        int testTimes = 1000;
+        int testTimes = 10000;
         int maxValue = 10000;
         String ope = "\n";
         for (int i = 0; i < testTimes; ++i) {
             double choice = StdRandom.uniform();
             if (choice < 0.2 && !stu.isEmpty() && !right.isEmpty()) {
-                int stuRemoveFirst = stu.removeFirst();
-                int rightRemoveFirst = right.removeFirst();
+                Integer stuRemoveFirst = stu.removeFirst();
+                Integer rightRemoveFirst = right.removeFirst();
                 ope += "removeFirst(): " + stuRemoveFirst + "\n";
                 assertEquals(ope, stuRemoveFirst, rightRemoveFirst);
             } else if (choice < 0.4 && !stu.isEmpty() && !right.isEmpty()) {
-                int stuRemoveLast = stu.removeLast();
-                int rightRemoveLast = right.removeLast();
+                Integer stuRemoveLast = stu.removeLast();
+                Integer rightRemoveLast = right.removeLast();
                 ope += "removeLast(): " + stuRemoveLast + "\n";
                 assertEquals(ope, stuRemoveLast, rightRemoveLast);
             } else if (choice < 0.7) {
