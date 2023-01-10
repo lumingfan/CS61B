27a51e322b29db80c0613452b5ff0c85e8868aa3
lumingfan <2507550027@qq.com>
2023-01-10 15:09:40 +0800

    add the the if statement to handle the empty situation

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -71,6 +71,9 @@ public class MergeSort {
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
         // Your code here!
+        if (items.isEmpty()) {
+            return items;
+        }
         Queue<Queue<Item>> queues = makeSingleItemQueues(items);
         while (queues.size() > 1) {
             queues.enqueue(mergeSortedQueues(queues.dequeue(), queues.dequeue()));
