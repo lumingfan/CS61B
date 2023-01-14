9c0f1412b0e219932078e8140c34bfccad0b5ace
lumingfan <2507550027@qq.com>
2023-01-14 23:53:34 +0800

    the partI test

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -8,10 +8,87 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
+    public static final double ROOT_ULLAT = 37.892195547244356, ROOT_ULLON = -122.2998046875,
+            ROOT_LRLAT = 37.82280243352756, ROOT_LRLON = -122.2119140625;
+    public static final int TILE_SIZE = 256;
+    private static final double ROOT_LONDPP =  (ROOT_LRLON - ROOT_ULLON) / TILE_SIZE;
+    private static final int MAX_DEPTH = 7;
 
     public Rasterer() {
         // YOUR CODE HERE
     }
+
+    // if pass test, the modifier should be private
+    public RequestInfo resolute(Map<String, Double> params) {
+        RequestInfo info = new RequestInfo();
+        for (Map.Entry<String, Double> entry : params.entrySet()) {
+            info.setInfoByEntry(entry);
+        }
+        return info;
+    }
+
+    // the following three function still not test, maybe false
+    private boolean validateLon(RequestInfo info) {
+        return info.getUllon() <= ROOT_LRLON && info.getLrlon() >= ROOT_ULLON;
+    }
+    private boolean validateLat(RequestInfo info) {
+        return info.getUllat() >= ROOT_LRLAT &&  info.getLrlat() <= ROOT_ULLAT;
+    }
+    private boolean validate(RequestInfo info) {
+        return validateLat(info) && validateLon(info);
+    }
+
+
+
+    private static double getRootLonDPP() { return ROOT_LONDPP; }
+
+    // if pass test, the modifier should be private
+    private static double getLonDPP(int depth) {
+        return getRootLonDPP() / Math.pow(2, depth);
+    }
+
+    public static int calDepth(double target_lonDPP) {
+        for (int i = 0; i <= MAX_DEPTH ; ++i) {
+            if (getLonDPP(i) < target_lonDPP) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int calTileNum(int depth) {
+        return (int) Math.pow(2, depth);
+    }
+
+    public int calIndex(double now, double root, double std) {
+        double dis = now - root;
+        if (dis < 0) {
+            return 0;
+        }
+        return (int) (dis / std);
+    }
+
+    public int calBeginRow(RequestInfo info, double dis) {
+        return calIndex(ROOT_ULLAT, info.getUllat(), dis);
+    }
+
+    public int calEndRow(RequestInfo info, double dis) {
+        return calIndex(ROOT_ULLAT, info.getLrlat(), dis);
+    }
+
+    public int calBeginCol(RequestInfo info, double dis) {
+        return calIndex(info.getUllon(), ROOT_ULLON, dis);
+    }
+
+    public int calEndCol(RequestInfo info, double dis) {
+        return calIndex(info.getLrlon(), ROOT_ULLON, dis);
+    }
+
+    private double calAveDis(double source, double target, int num) {
+        return (target - source) / num;
+    }
+
+
 
     /**
      * Takes a user query and finds the grid of images that best matches the query. These
@@ -42,10 +119,47 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+
+        RequestInfo paramsInfo = resolute(params);
+        int depth = calDepth(paramsInfo.getLonDPP());
+        boolean query_success = (depth != -1 && validate(paramsInfo));
+
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        results.put("query_success", query_success);
+        results.put("depth", depth);
+        if (!query_success) {
+            return results;
+        }
+
+        int tileNum = calTileNum(depth);
+        double aveLatDis = calAveDis(ROOT_LRLAT, ROOT_ULLAT, tileNum);
+        double aveLonDis = calAveDis(ROOT_ULLON, ROOT_LRLON, tileNum);
+
+        int beginRow = calBeginRow(paramsInfo, aveLatDis);
+        int endRow = calEndRow(paramsInfo, aveLatDis);
+        int beginCol = calBeginCol(paramsInfo, aveLonDis);
+        int endCol = calEndCol(paramsInfo, aveLonDis);
+
+        int rows = endRow - beginRow + 1;
+        int cols = endCol - beginCol + 1;
+        String[][] tiles = new String[rows][cols];
+        for (int row = beginRow; row <= endRow; ++row) {
+            for (int col = beginCol; col <= endCol; ++col) {
+                tiles[row][col] = "d" + depth + "_x" + row + "_y" + col + ".png";
+            }
+        }
+
+        double raster_ul_lon = ROOT_ULLON + beginCol * aveLonDis;
+        double raster_lr_lon = ROOT_ULLON + endCol * aveLonDis;
+        double raster_ul_lat = ROOT_ULLAT - beginRow * aveLatDis;
+        double raster_lr_lat = ROOT_ULLAT - endRow * aveLatDis;
+
+        results.put("raster_ul_lon", raster_ul_lon);
+        results.put("raster_lr_lon", raster_lr_lon);
+        results.put("raster_ul_lat", raster_ul_lat);
+        results.put("raster_lr_lat", raster_lr_lat);
+        results.put("render_grid", tiles);
+
         return results;
     }
 
---
+++ proj3/src/main/java/RequestInfo.java
@@ -0,0 +1,99 @@
+import java.util.Map;
+public class RequestInfo {
+    private static final String[] PARAMS ={"lrlon", "ullon", "lrlat", "ullat", "w", "h"};
+    private double lrlon;
+    private double ullon;
+    private double lrlat;
+    private double ullat;
+    private double w;
+    private double h;
+
+    private double lonDPP;
+    private boolean lonDPP_calculated;
+
+    public RequestInfo() {}
+
+    public RequestInfo(double lrlon, double ullon, double lrlat, double ullat, double w, double h) {
+        this.lrlon = lrlon;
+        this.ullon = ullon;
+        this.lrlat = lrlat;
+        this.ullat = ullat;
+        this.w = w;
+        this.h = h;
+        this.lonDPP = 0.0;
+        this.lonDPP_calculated = false;
+    }
+
+    public double getUllat() {
+        return ullat;
+    }
+
+    public void setUllat(double ullat) {
+        this.ullat = ullat;
+    }
+
+    public double getLrlon() {
+        return lrlon;
+    }
+
+    public void setLrlon(double lrlon) {
+        this.lrlon = lrlon;
+    }
+
+    public double getUllon() {
+        return ullon;
+    }
+
+    public void setUllon(double ullon) {
+        this.ullon = ullon;
+    }
+
+    public double getLrlat() {
+        return lrlat;
+    }
+
+    public void setLrlat(double lrlat) {
+        this.lrlat = lrlat;
+    }
+
+    public double getW() {
+        return w;
+    }
+
+    public void setW(double w) {
+        this.w = w;
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
+
+    public void setInfoByEntry(Map.Entry<String, Double> entry) {
+
+        String item = entry.getKey();
+        double value = entry.getValue();
+        if (item.equals(PARAMS[0])) { setLrlon(value); }
+        else if (item.equals(PARAMS[1])) { setUllon(value); }
+        else if (item.equals(PARAMS[2])) { setLrlat(value); }
+        else if (item.equals(PARAMS[3])) { setUllat(value); }
+        else if (item.equals(PARAMS[4])) { setW(value); }
+        else if (item.equals(PARAMS[5])) { setH(value); }
+    }
+
+    private double calLonDPP() {
+        return (lrlon - ullon) / w;
+    }
+
+    public double getLonDPP() {
+        if (lonDPP_calculated) {
+            return lonDPP;
+        }
+        lonDPP = calLonDPP();
+        lonDPP_calculated = true;
+        return lonDPP;
+    }
+}
