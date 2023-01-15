fb3e7182ba9f68a6bfaa60ca8899249cada8d70a
lumingfan <2507550027@qq.com>
2023-01-15 17:01:15 +0800

    Way class: store the way info in OSMXMl, provides some helper to GraphBuildingHandler.endElement()

--- proj3/src/main/java/Way.java
+++ proj3/src/main/java/Way.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Way {
+import java.util.LinkedList;
+import java.util.List;
+
+public class Way {
+    private List<Node> way;
+    private String name;
+
+    private boolean flag;
+    public Way() {
+        flag = false;
+        this.way = new LinkedList<>();
+    }
+
+
+    public List<Node> getWay() {
+        return way;
+    }
+
+    public void addNextStop(Node node) {
+        way.add(node);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
