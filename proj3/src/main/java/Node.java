2b3c9b464322972084fd07f673d804c2a94ce693
lumingfan <2507550027@qq.com>
2023-01-15 16:59:31 +0800

    Node class: store the node info in OSM XML and provide some helper function to GraphDB: adjacent, lon, lat....

--- proj3/src/main/java/Node.java
+++ proj3/src/main/java/Node.java
@@ -1,2 +1,74 @@
-package PACKAGE_NAME;public class Node {
+import javax.swing.text.Position;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Node {
+    private long id;
+    private double lat;
+    private double lon;
+    private String name;
+    private Set<Edge> edgeSet;
+
+    public Node(long id, double lat, double lon) {
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+        this.edgeSet = new HashSet<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addEdge(Edge edge) {
+        edgeSet.add(edge);
+    }
+
+    public boolean isLinked() {
+        return !edgeSet.isEmpty();
+    }
+
+    public Iterable<Long> adjacent() {
+        Set<Long> adjNodeId = new HashSet<>();
+        for (Edge edge : edgeSet) {
+            Node nodeOne = edge.getNodeOne();
+            Node nodeTwo = edge.getNodeTwo();
+            if (nodeOne != this) {
+                adjNodeId.add(nodeOne.getId());
+            } else {
+                adjNodeId.add(nodeTwo.getId());
+            }
+        }
+        return adjNodeId;
+    }
+
+
 }
